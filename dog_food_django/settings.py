383105f5c1b0f345ce0e6351513da3fc54fc26bf
Michael Aleksa <mtaleksa1@gmail.com>
2020-01-15 20:03:19 -0500

    create models for food and diet

--- dog_food_django/settings.py
+++ dog_food_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'food_search.apps.FoodSearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- food_search/models.py
+++ food_search/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Food(models.Model):
+    item_num = models.IntegerField('Item Number', primary_key=True)
+    url = models.CharField('URL', max_length=200, unique=True)
+    ingredients = models.CharField('Ingredients', max_length=4096, null=False)
+    brand = models.CharField('Brand', max_length=200, null=True)
+    xsm_breed = models.BooleanField('Extra Small & Toy Breeds')
+    sm_breed = models.BooleanField('Small Breeds')
+    md_breed = models.BooleanField('Medium Breeds')
+    lg_breed = models.BooleanField('Large Breeds')
+    xlg_breed = models.BooleanField('Giant Breeds')
+    food_form = models.CharField('Food Form', max_length=200, null=True)
+    lifestage = models.CharField('Lifestage', max_length=200, null=True)
+    fda_guidelines = models.BooleanField('Adheres to FDA Guidelines')
+
+
+class Diet(models.Model):
+    diet = models.CharField('Diet', max_length=200)
+    item_num = models.ForeignKey(Food, on_delete=models.CASCADE)
