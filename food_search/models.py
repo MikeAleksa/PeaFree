9d5bb132cbed62c72e3ee805706a19b9e3daea98
Michael Aleksa <mtaleksa1@gmail.com>
2020-01-15 20:32:44 -0500

    add __str__ methods to food_search/models

--- food_search/models.py
+++ food_search/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
+
+    def __str__(self):
+        return self.url
+
     item_num = models.IntegerField('Item Number', primary_key=True)
     url = models.CharField('URL', max_length=200, unique=True)
     ingredients = models.CharField('Ingredients', max_length=4096, null=False)
@@ -18,5 +22,9 @@ class Food(models.Model):
 
 
 class Diet(models.Model):
+
+    def __str__(self):
+        return '{} (item number {})'.format(self.diet, self.item_num.item_num)
+
     diet = models.CharField('Diet', max_length=200)
     item_num = models.ForeignKey(Food, on_delete=models.CASCADE)
