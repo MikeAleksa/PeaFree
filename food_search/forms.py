f271b1be802716c5592a88ae717914291a5b2057
Michael Aleksa <mtaleksa1@gmail.com>
2020-01-20 15:05:10 -0500

    mostly fix issue with footer not appearing in correct place on results page

--- food_search/forms.py
+++ food_search/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    q = forms.CharField(max_length=100, required=False)
+    q = forms.CharField(max_length=100, required=False, label='', )
     fda = forms.BooleanField(required=False, initial=True, label='Meets FDA Guidelines')
