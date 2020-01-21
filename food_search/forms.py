c1902dec00ff542610268518d37fcc1ab0fe8806
Michael Aleksa <mtaleksa1@gmail.com>
2020-01-20 23:40:18 -0500

    add breed selection in search

--- food_search/forms.py
+++ food_search/forms.py
@@ -3,4 +3,14 @@ from django import forms
 
 class SearchForm(forms.Form):
     q = forms.CharField(max_length=100, required=False, label='', )
-    fda = forms.BooleanField(required=False, initial=True, label='Meets FDA Guidelines')
+    fda = forms.BooleanField(required=False, label='Meets FDA guidelines', initial=True)
+    # breed_choices = Food.objects.order_by('breed').values_list('breed', flat=True).distinct()
+
+    breed_choices = enumerate(['Any',
+                               'Extra Small & Toy Breeds',
+                               'Small Breeds',
+                               'Medium Breeds',
+                               'Large Breeds',
+                               'Giant Breeds',
+                               ])
+    breed_size = forms.ChoiceField(choices=breed_choices)
--- food_search/views.py
+++ food_search/views.py
@@ -29,6 +29,9 @@ class ResultsView(generic.ListView):
     def get_fda(self):
         return self.request.GET.get('fda', 'on')
 
+    def get_breed_size(self):
+        return self.request.GET.get('breed_size', None)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
@@ -43,15 +46,28 @@ class ResultsView(generic.ListView):
 
     def get_queryset(self):
         queryset = Food.objects.all()
-        query_list = self.get_q()
 
         # if a search query was made, split it into words and filter
+        query_list = self.get_q()
         if query_list:
             query_list = query_list.split()
             queryset = queryset.filter(reduce(operator.and_, (Q(name__icontains=q) for q in query_list)))
 
         # filter by FDA guidelines
-        if self.request.GET.get('fda') == 'on':
+        if self.get_fda() == 'on':
             queryset = queryset.filter(fda_guidelines=True)
 
+        # filter by Breeds
+        if self.get_breed_size() == '1':
+            queryset = queryset.filter(xsm_breed=True)
+        elif self.get_breed_size() == '2':
+            queryset = queryset.filter(sm_breed=True)
+        elif self.get_breed_size() == '3':
+            queryset = queryset.filter(md_breed=True)
+        elif self.get_breed_size() == '4':
+            queryset = queryset.filter(lg_breed=True)
+        elif self.get_breed_size() == '5':
+            queryset = queryset.filter(xlg_breed=True)
+
+        # return ordered queryset
         return queryset.order_by('name')
