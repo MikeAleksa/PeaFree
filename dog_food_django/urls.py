56a382b593cf4730ba9fefe6c2780ffa62a72ade
Michael Aleksa <mtaleksa1@gmail.com>
2020-01-23 03:58:36 -0500

    redirect index page to index of food_search

--- dog_food_django/urls.py
+++ dog_food_django/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda r: HttpResponseRedirect('search/')),
     path('admin/', admin.site.urls),
     path('search/', include('food_search.urls'))
 ]
